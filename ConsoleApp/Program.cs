c149ec85999101954e2849532e9a5fe81ba540b2
Fahmi Sarker <abir091@hotmail.com>
2018-08-31 07:56:24 +1000

    Adding binary search both iterative and recursive

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -12,78 +12,31 @@ using System.Text.RegularExpressions;
 using System.Text;
 using System;
 using ConsoleApp1;
+using Implementation.Search;
 
 class Solution
 {
-    // Complete the minimumAbsoluteDifference function below.
-    // Complete the whatFlavors function below.
-    static void whatFlavors(int[] cost, int money)
-    {
-        Tuple<int, int>[] newCost = new Tuple<int, int>[cost.Length];
-        for (int i = 0; i < cost.Length; i++)
-        {
-            newCost[i] = new Tuple<int, int>(cost[i], i);
-        }
-
-        Tuple<int, int>[] sortedCost = newCost.OrderBy(o => o.Item1).ToArray();
-        int one =0;
-        int two =0; 
-        for (int i = 0; i < sortedCost.Length; i++)
-        {
-            one = i;
-            two = findIndex(sortedCost, money - cost[one]);
-
-            if (two != -1 && two != one)
-                break;
-        }
-        
-        if(one < two)
-            Console.WriteLine("{0} {1}",one + 1, two +1 );
-        else
-            Console.WriteLine("{0} {1}",two + 1, one + 1);
-    }
-
-    static int findIndex(Tuple<int, int>[] a,int searchValue) {
-        int left = 0;
-        int right = a.Length - 1;
-
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (a[mid].Item1 == searchValue)
-            {
-                return  a[mid].Item2;
-            }
-            else if (searchValue < a[mid].Item1)
-            {
-                right = mid - 1;
-            }
-            else {
-                left = mid + 1;
-            }
-        }
-        return -1;
-    }
+  
     static void Main(string[] args)
     {
         //Console.WriteLine(minimumAbsoluteDifference(new int[] { -59, -36, -13, 1, -53, -92, -2, -96, -54, 75 }));
 
-        //var a = TextFileReader.GetArray(@"C:\Temp\input.txt");
-        var s = File.ReadLines(@"C:\Temp\input.txt").Select(r=>r.TrimEnd()).ToArray();
+        //var s = File.ReadLines(@"C:\Temp\input.txt").Select(r=>r.TrimEnd()).ToArray();
 
-        int t = int.Parse(s[0]);
+        //int t = int.Parse(s[0]);
 
-        for (int tItr = 0; tItr < t; tItr++)
-        {
-            int money = Convert.ToInt32(s[tItr * 3 + 1]);
+        //for (int tItr = 0; tItr < t; tItr++)
+        //{
+        //    int money = Convert.ToInt32(s[tItr * 3 + 1]);
 
-            int n = Convert.ToInt32(s[tItr * 3 + 2]);
+        //    int n = Convert.ToInt32(s[tItr * 3 + 2]);
 
-            int[] cost = s[tItr * 3 + 3].Split(' ').Select(r => int.Parse(r)).ToArray();
-            ;
-            whatFlavors(cost, money);
-        }
+        //    int[] cost = s[tItr * 3 + 3].Split(' ').Select(r => int.Parse(r)).ToArray();
+            
+        //    //whatFlavors(cost, money);
+        //}
 
-      //  whatFlavors(a, 245589753);
+        Console.WriteLine(BinarySearch.BinarySearchRecursive(new int[] { 3,3,4,2, 2, 5, 1 }, 5,-1,0,6));
 
        
     }
---
+++ Implementation/Search/BinarySearch.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Implementation.Search
+{
+    public class BinarySearch
+    {
+        /// <summary>
+        /// Binary search compares the target value to the middle element of the array. 
+        /// If they are not equal, the half in which the target cannot lie is eliminated and 
+        /// the search continues on the remaining half, again taking the middle element 
+        /// to compare to the target value, and repeating this until the target value is found.
+        /// </summary>
+        /// <param name="a">The SORTED array that needs to be searched</param>
+        /// <param name="value">The value that we are searching. </param>
+        /// <returns>Index of the item if found. -1 if not found.</returns>
+        /// <example>BinarySearchIterative(new int[]{3,5,2,5,1},5)</example>
+        public static int BinarySearchIterative(int[] a, int value) {
+            int left = 0;
+            int right = a.Length - 1;
+            while (left < right) {
+                int mid = left + (right - left) / 2;
+
+                if (a[mid].Equals(value))
+                    return mid;
+                else if (value < a[mid])
+                {
+                    right = mid - 1;
+                }
+                else {
+                    left = mid + 1;
+                }
+            }
+            return -1;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="a">Array to search</param>
+        /// <param name="value">Value to look for</param>
+        /// <param name="index">Initial index. Default to -1</param>
+        /// <param name="left">Left search bound index. Start with 0</param>
+        /// <param name="right">Right search bound index. Start with a.Length - 1</param>
+        /// <returns>Index of array a if found. -1 if not (assuming you default index to -1)</returns>
+        /// <example>BinarySearchRecursive(new int[] { 3,3,4,2, 2, 5, 1 }, 5,-1,0,6))</example>
+        public static int BinarySearchRecursive(int[] a, int value, int index, int left, int right) {
+
+            if (left <= right)
+            {
+                int mid = left + ((right - left) / 2);
+
+                // If value is found
+                if (value == a[mid])
+                {
+                    index = mid;
+                }
+                else
+                {
+                    // if value < array[midIndex], search left
+                    if (value < a[mid])
+                    {
+                        right = mid - 1;
+                    }
+                    else
+                    { // if value > array[midIndex], search right
+                        left = mid + 1;
+                    }
+
+                    index = BinarySearchRecursive(a, value, index, left, right);
+                }
+            }
+
+            return index;
+        }
+    }
+}
